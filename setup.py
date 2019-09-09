b20da921798946d75366d43599469e3abeae611d
Gabor Barnak <gabor.barnak@gmail.com>
2019-09-09 13:46:06 +0200

    chore: initial commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='spike_airflow_plugins',
+    version='0.1.0',
+    packages=find_packages(exclude='tests'),
+    test_suite='tests',
+    entry_points={
+        'airflow.plugins': [
+            'my_namespace = spike_airflow_plugins.my_plugin:MyAirflowPlugin'
+        ],
+    },
+)
