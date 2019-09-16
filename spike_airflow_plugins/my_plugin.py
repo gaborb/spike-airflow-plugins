5f175c9b6e98ecc86b06d6d70ce3f9a0275e2703
Gabor Barnak <gabor.barnak@gmail.com>
2019-09-16 15:32:22 +0200

    chore: add pylintrc and fix lint errors

--- spike_airflow_plugins/my_plugin.py
+++ spike_airflow_plugins/my_plugin.py
@@ -1,22 +1,23 @@
 from airflow.plugins_manager import AirflowPlugin
 
 from airflow.contrib.hooks.bigquery_hook import BigQueryHook
-from googleapiclient.errors import HttpError
 from airflow.utils.decorators import apply_defaults
 # from airflow.models import BaseOperator
 from airflow.sensors.base_sensor_operator import BaseSensorOperator
+from googleapiclient.errors import HttpError
 
 
+# pylint: disable=abstract-method
 class MyHook(BigQueryHook):
     def is_job_done(self, project_id, job_id):
         service = self.get_service()
         try:
             response = service.jobs().get(projectId=project_id, jobId=job_id).execute()
-            state = response['status']['state']
-            self.log.info('Status state of job is: %s', state)
-            return state == 'DONE'
-        except HttpError as e:
-            if e.resp['status'] == '404':
+            state = response["status"]["state"]
+            self.log.info("Status state of job is: %s", state)
+            return state == "DONE"
+        except HttpError as error:
+            if error.resp["status"] == "404":
                 return False
             raise
 
@@ -26,24 +27,24 @@ class MyHook(BigQueryHook):
 
 
 class MySensorOperator(BaseSensorOperator):
-    template_fields = ('project_id', 'job_id')
-    ui_color = '#f0eee4'
+    template_fields = ("project_id", "job_id")
+    ui_color = "#f0eee4"
 
     @apply_defaults
     def __init__(self,
                  job_id,
                  project_id=None,
-                 bigquery_conn_id='bigquery_default',
+                 bigquery_conn_id="bigquery_default",
                  delegate_to=None,
-                 *args, **kwargs):
-        super(MySensorOperator, self).__init__(*args, **kwargs)
+                 **kwargs):
+        super(MySensorOperator, self).__init__(**kwargs)
         self.project_id = project_id
         self.job_id = job_id
         self.bigquery_conn_id = bigquery_conn_id
         self.delegate_to = delegate_to
 
     def poke(self, context):
-        self.log.info('Sensor checks status state of job: %s', self.job_id)
+        self.log.info("Sensor checks status state of job: %s", self.job_id)
         hook = MyHook(bigquery_conn_id=self.bigquery_conn_id, delegate_to=self.delegate_to)
         return hook.is_job_done(self.project_id, self.job_id)
 
